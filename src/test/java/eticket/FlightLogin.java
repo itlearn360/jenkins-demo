d7c20599d7abe2f39dfbc4f941f40642baa99a1f
itlearn360 <40198951+itlearn360@users.noreply.github.com>
2018-12-18 13:24:02 +0530

    update chrome options

--- src/test/java/eticket/FlightLogin.java
+++ src/test/java/eticket/FlightLogin.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
@@ -16,7 +17,15 @@ public class FlightLogin {
 	@BeforeTest
 	public void init(){
 		System.setProperty("webdriver.chrome.driver", "/var/www/tool/chromedriver");
-	  driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("start-maximized"); // open Browser in maximized mode
+		options.addArguments("disable-infobars"); // disabling infobars
+		options.addArguments("--disable-extensions"); // disabling extensions
+		options.addArguments("--disable-gpu"); // applicable to windows os only
+		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+		options.addArguments("--no-sandbox"); // Bypass OS security model
+		options.addArguments("--headless");
+	  driver = new ChromeDriver(options);
 	  url = "http://newtours.demoaut.com";
 	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 	}
